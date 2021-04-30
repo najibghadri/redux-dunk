c93c1f8a2f4a9abc364d7c8b8df5a8af6867b4a8
Najib Ghadri <najib.ghadri@prezi.com>
2021-04-30 17:14:09 +0200

    Update dunkTest.ts

--- dunkTest.ts
+++ dunkTest.ts
@@ -29,24 +29,23 @@ const fetchUser = EffectCreator((userId: number) => async ({ dispatch }) => {
         .catch(_ => dispatch(actions.failedAction()));
 });
 
-test('success case', () => {
-    const store = configureStore<State>([dunkMiddleware])(initialState);
+const store = configureStore<State>([dunkMiddleware])(initialState);
+const mockStoreApi = { dispatch: store.dispatch, getState: store.getState };
 
+test('success case', () => {
     fails = false;
     const expectedAction = actions.successfulAction('hello');
 
-    fetchUser(1)({ dispatch: store.dispatch, getState: store.getState }).then(() => {
+    fetchUser(1)(mockStoreApi).then(() => {
         expect(store.getActions()).toEqual([expectedAction]);
     });
 });
 
 test('fail case', () => {
-    const store = configureStore<State>([dunkMiddleware])(initialState);
-
     fails = true;
     const expectedAction = actions.failedAction();
 
-    fetchUser(1)({ dispatch: store.dispatch, getState: store.getState }).then(() => {
+    fetchUser(1)(mockStoreApi).then(() => {
         expect(store.getActions()).toEqual([expectedAction]);
     });
 });
